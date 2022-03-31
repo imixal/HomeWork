736f945930fdb9d3acfa7aad6cf49d055326fb6b
Avetisovave <avetisov-v@mail.ru>
2022-03-31 21:21:43 +0300

    add Scripts

---
+++ Assets/Runner3D/Scripts/Core/IGameContext.cs
@@ -0,0 +1,15 @@
+﻿using Common.Scripts.Service;
+
+namespace Runner3D.Scripts.Core
+{
+    public interface IGameContext
+    {
+        IAudioService AudioService { get;}
+        ISaveService SaveService { get; }
+        ISceneService SceneService { get; }
+        ICameraService CameraService { get; }
+
+        void ShowView(string viewName);
+        void HideView();
+    }
+}
