6a499b20be850d61d36608d83c140a57bfd77559
Avetisovave <avetisov-v@mail.ru>
2022-03-31 21:38:30 +0300

    add My Player

--- Assets/Platformer/MainPlayer/Scripts/PlayerView.cs
+++ Assets/Platformer/MainPlayer/Scripts/PlayerView.cs
@@ -1,22 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Platformer.Objects.Coin;
 using UnityEngine;
 
 namespace Platformer.MainPlayer.Scripts
 {
-	public class PlayerView : MonoBehaviour
-	{
-		public event Action CoinCollected;
+    public class PlayerView : MonoBehaviour
+    {
+        public event Action CoinCollected;
 
-		private void OnTriggerEnter2D(Collider2D other)
-		{
-			var coin = other.gameObject.GetComponent<CoinView>();
-			if (coin != null)
-			{
-				Destroy(coin.gameObject);
-				CoinCollected.Invoke();
-			}
-		}
-	}
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var coin = other.gameObject.GetComponent<CoinView>();
+            if (coin != null)
+            {
+                Destroy(coin.gameObject);
+                CoinCollected.Invoke();
+            }
+        }
+    }
 }
